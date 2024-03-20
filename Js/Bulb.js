071593e464f364c21272c74e537e414b5616f2de
Gaurav Jain <gjain6203@gmail.com>
2024-03-20 22:14:45 +0530

    add some file

---
+++ Js/Bulb.js
@@ -0,0 +1,6 @@
+function myfunon(){
+    document.getElementById("buld").src="/html/lamp-idea-icon-object-yellow-light-white-background-vector-illustration_3482-3989.avif";
+}
+function myfunoff(){
+    document.getElementById("buld").src="/html/360_F_547033251_2OJW4xblTyjOnMN8GPRO4Qk5inSsgNQC.jpg";
+}
